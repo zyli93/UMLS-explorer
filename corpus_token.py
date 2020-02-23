d0acfd8bfbe17801658d0154a713470edbdb388c
Louis Qin <qyl0509@icloud.com>
2020-02-23 08:33:25 +0000

    add stemming and lemmatization

--- corpus_token.py
+++ corpus_token.py
@@ -1,13 +1,27 @@
 import os
-import pandas as pd
+import pickle
 import nltk
 from nltk.tokenize import word_tokenize
+from nltk.stem import PorterStemmer, WordNetLemmatizer
 
-def stemming_lemmatisation():
-    # TODO: implement stemming and lemmatisation
-    return
+def stem(tokens):
+    """stem a list of raw tokens"""
+    root_tokens = []
+    ps = PorterStemmer()
+    for w in tokens:
+        root_tokens += ps.stem(w)
+    return root_tokens
+
+def lemmatize(tokens):
+    """lemmatize a list of raw tokens"""
+    root_tokens = []
+    wl = WordNetLemmatizer()
+    for w in tokens:
+        root_tokens += wl.lemmatize(w)
+    return root_tokens
 
 def pubmed_token():
+    """tokenize pubmed corpus"""
     tokens = []
     path_to_medline = "../medline_output"
     with os.scandir(path_to_medline) as files:
@@ -18,10 +32,19 @@ def pubmed_token():
     return tokens
 
 def pmc_token():
+    """tokenize pmc corpus"""
     # TODO: handle pmc format
     return
 
 if __name__ == "__main__":
-    pubmed_tokens = set(pubmed_token())
-    with open('pubmed_tokens.pickle', 'wb') as handle:
-        pickle.dump(pubmed_tokens, handle, protocol=pickle.HIGHEST_PROTOCOL)
+    pubmed_raw_tokens = pubmed_token()
+    with open('pubmed_raw_tokens.pickle', 'wb') as handle:
+        pickle.dump(set(pubmed_raw_tokens), handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+    pubmed_stemmed_tokens = stem(pubmed_raw_tokens)
+    with open('pubmed_stemmed_tokens.pickle', 'wb') as handle:
+        pickle.dump(set(pubmed_stemmed_tokens), handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+    pubmed_lemmatized_tokens = lemmatize(pubmed_raw_tokens)
+    with open('pubmed_lemmatized_tokens.pickle', 'wb') as handle:
+        pickle.dump(set(pubmed_lemmatized_tokens), handle, protocol=pickle.HIGHEST_PROTOCOL)
