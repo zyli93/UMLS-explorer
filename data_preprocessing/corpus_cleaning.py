0da8d9bcfc37d39221413f8b20175747ea9c879f
Louis Qin <qyl0509@icloud.com>
2020-02-27 03:57:55 +0000

    add pmc cleaning; support mp

--- data_preprocessing/corpus_cleaning.py
+++ data_preprocessing/corpus_cleaning.py
@@ -16,7 +16,7 @@
 import os
 import re
 import pickle
-from collections import Counter
+import multiprocessing
 from nltk.tokenize import word_tokenize, sent_tokenize
 from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
@@ -24,45 +24,61 @@ from nltk import FreqDist
 
 MEDLINE_SRC_DIR = "../../medline_output"
 MEDLINE_DEST_DIR = "../../medline_clean"
+MEDLINE_FREQ_DIR = "../../medline_freq"
+PMC_SRC_DIR = "../../pmc_output"
+PMC_DEST_DIR = "../../pmc_clean"
+PMC_FREQ_DIR = "../../pmc_freq"
 
-def sentence_cleaning(sentence):
-    """return a cleaned list of words from a sentence"""
-    # lowercase sentences
-    lowered = sentence.lower()
-    # tokenize word
-    words = word_tokenize(lowered)
-    # remove punctuation and stop words
-    cleaned = [w for w in words if re.search('[a-zA-Z]', w) and not w in stopwords.words('english')]
-    # lemmatize words
-    wl = WordNetLemmatizer()
-    lemmas = [wl.lemmatize(w) for w in cleaned]
-    return lemmas
+def file_cleaning(src_dir, dest_dir, freq_dir):
+    """clean a file and count clean words frequencies"""
+    print(f"processing {src_dir}...")
+    freq = FreqDist([])
+    with open(src_dir, 'r') as f:
+        paragraph = f.read()
+    sentences = sent_tokenize(paragraph)
+    for s in sentences:
+        # Special lines in pmc
+        if s.startswith(">>>>>>"):
+            s = s.split(None, 1)[1]
+        # lowercase sentences
+        lowered = s.lower()
+        # tokenize word
+        words = word_tokenize(lowered)
+        # remove punctuation and stop words
+        cleaned = [w for w in words if re.search('[a-zA-Z]', w) and not w in stopwords.words('english')]
+        # lemmatize words
+        wl = WordNetLemmatizer()
+        lemmas = [wl.lemmatize(w) for w in cleaned]
+        # write cleaned sentences
+        with open(dest_dir, 'a+') as fout:
+            print(' '.join(lemmas), file=fout)
+        # update word frequency
+        freq += FreqDist(lemmas)
+    with open(f'{freq_dir}.pickle', 'wb') as handle:
+        pickle.dump(freq, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
 def medline_cleaning():
     """clean medline corpus"""
-    freq = FreqDist([])
     with os.scandir(MEDLINE_SRC_DIR) as files:
-        for file in files:
-            src_dir = os.path.join(MEDLINE_SRC_DIR, file.name)
-            dest_dir = os.path.join(MEDLINE_DEST_DIR, file.name)
-            print("processing {}...".format(src_dir))
-            with open(src_dir, 'r') as f:
-                paragraph = f.read()
-            sentences = sent_tokenize(paragraph)
-            for s in sentences:
-                lemmas = sentence_cleaning(s)
-                # write cleaned sentences
-                with open(dest_dir, 'a+') as fout:
-                    print(' '.join(lemmas), file=fout)
-                # update word frequency
-                freq += FreqDist(lemmas)
-    with open('medline_frequency.pickle', 'wb') as handle:
-        pickle.dump(freq, handle, protocol=pickle.HIGHEST_PROTOCOL)
+        dir_args = [(os.path.join(MEDLINE_SRC_DIR, file.name), \
+                    os.path.join(MEDLINE_DEST_DIR, file.name), \
+                    os.path.join(MEDLINE_FREQ_DIR, file.name)) for file in files]
+    with multiprocessing.Pool() as pool:
+        pool.starmap(file_cleaning, dir_args)
 
 def pmc_cleaning():
     """clean pmc corpus"""
-    # TODO: handle pmc format
-    return
+    with os.scandir(PMC_SRC_DIR) as files:
+        dir_args = [(os.path.join(PMC_SRC_DIR, file.name), \
+                    os.path.join(PMC_DEST_DIR, file.name), \
+                    os.path.join(PMC_FREQ_DIR, file.name)) for file in files]
+    with multiprocessing.Pool() as pool:
+        pool.starmap(file_cleaning, dir_args)
 
 if __name__ == "__main__":
-    medline_cleaning()
+    p1 = multiprocessing.Process(target=medline_cleaning)
+    p1.start()
+    p2 = multiprocessing.Process(target=pmc_cleaning)
+    p2.start()
+    p1.join()
+    p2.join()
