ac57ba2eb1502330cccdde0e4ce6064111cfc429
Louis Qin <qyl0509@icloud.com>
2020-11-05 09:01:01 +0000

    update transitive_closure.py to take args

--- data_preprocessing/transitive_closure.py
+++ data_preprocessing/transitive_closure.py
@@ -4,22 +4,37 @@
     Author: Louis Qin <louisqin@ucla.edu> or <qyl0509@icloud.com>
 """
 
+import os
+import argparse
 import pandas as pd
 
-mrhier_filled = pd.read_csv('../MRHIER/MRHIER_filled.csv', sep='|', dtype=object)
+def create_transitive_closure(mrhier_path, closure_dir, source_vocab):
+    """Create a transitive closure of (hyponym, hypernym)"""
+    mrhier = pd.read_csv(mrhier_path, sep='|', dtype=object)
 
-source = mrhier_filled[mrhier_filled['SAB'] == 'ICD10']
+    source = mrhier[mrhier['SAB'] == source_vocab]
 
-edges = set()
-for index, row in source.iterrows():
-    if pd.isna(row['PTR']):
-        continue
-    parents = row['PTR'].split('.')
-    for i, p in enumerate(parents):
-        for c in parents[1:]:
-            edges.add((c, p))
-        edges.add((row['AUI'], p))
+    edges = set()
+    for index, row in source.iterrows():
+        if pd.isna(row['PTR']):
+            continue
+        parents = row['PTR'].split('.')
+        for i, p in enumerate(parents):
+            for c in parents[1:]:
+                edges.add((c, p))
+            edges.add((row['AUI'], p))
 
-icd10 = pd.DataFrame(list(edges), columns=['id1', 'id2'])
-icd10['weight'] = 1
-icd10.to_csv('icd10_closure.csv', index=False)
+    closure = pd.DataFrame(list(edges), columns=['id1', 'id2'])
+    closure['weight'] = 1
+    closure.to_csv(os.path.join(closure_dir, f"{source_vocab}.csv"), index=False)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='transitive closure')
+    parser.add_argument('-v', '--source_vocab', required=True, help='UMLS source vocabulary SAB')
+    opt = parser.parse_args()
+
+    data_dir = os.environ['DATA_DIR']
+    mrhier_path = os.path.join(data_dir, 'hyperbolic', 'MRHIER_filled.csv')
+    closure_dir = os.path.join(data_dir, 'hyperbolic', 'transitive_closure')
+
+    create_transitive_closure(mrhier_path, closure_dir, opt.source_vocab)
